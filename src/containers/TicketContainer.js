022226b79165d788a96bd436de5e6661d084f33e
INA <codechemi@gmail.com>
2022-08-22 09:41:12 +0900

    주석 작성.

--- src/containers/TicketContainer.js
+++ src/containers/TicketContainer.js
@@ -1,6 +1,6 @@
 // 컨테이너 컴포넌트
-// 리덕스 스토어 상태를 조회하거나, 액션을 디스패치(발생시키는 것)할 수 있는 컴포넌트를 의미.
-// 다른 프로젠테이셔널 컴포넌트들을 불러와서 사용.
+// 리덕스 스토어 상태를 조회하거나, 액션을 디스패치(발생시키는 것)할 수 있는 컴포넌트를 의미합니다.
+// 다른 프로젠테이셔널 컴포넌트들을 불러와서 사용합니다.
 import React from "react";
 import { useSelector, useDispatch, shallowEqual } from "react-redux";
 import Ticket from "../components/Ticket";
@@ -20,7 +20,7 @@ import {
 } from "../modules/ticket";
 
 function TicketContainer() {
-  // 해당 값이 중 어느 하나라도 변경되면 렌더링
+  // 해당 값이 중 어느 하나라도 변경되면 렌더링합니다.
   const {
     airline,
     seatclass,
@@ -50,11 +50,11 @@ function TicketContainer() {
       seat: state.ticket.seat,
       gate: state.ticket.gate,
     }),
-    // react-redux에 내장되어있는 함수로서, 객체 안의 가장 겉에 있는 값들을 모두 비교해준다.
+    // react-redux에 내장되어있는 함수로서, 객체 안의 가장 겉에 있는 값들을 모두 비교해줍니다.
     shallowEqual
   );
 
-  // useDispatch는 리덕스 스토의 dispatch를 함수에서 사용 할 수 있게 해주는 Hook.
+  // useDispatch는 리덕스 스토의 dispatch를 함수에서 사용 할 수 있게 해주는 훅(Hook)입니다.
   const dispatch = useDispatch();
 
   const setAirLine = (AirLine) => dispatch(SetAirLine(AirLine));
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
-//루트 리듀서 : 한 프로젝트에 여러개의 리듀서가 있을때 하나로 합칠때 사용.
+// 루트 리듀서(Root Reducer)
+// 한 프로젝트에 여러개의 리듀서가 있을때 하나로 합칠때 사용합니다.
 import ticket from "./ticket";
 
 const rootReducer = combineReducers({
